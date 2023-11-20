f11b6f1ac5d2df91c74841b6c363f35d028875ac
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-20 13:09:21 -0500

    Update turnToPositionSteer

--- src/main/java/frc/robot/subsystems/drive/Steer.java
+++ src/main/java/frc/robot/subsystems/drive/Steer.java
@@ -44,7 +44,10 @@ public abstract class Steer {
     public void turnToPosition(double degrees) {
         // TODO: create a double called measurementRadians and initialize to Math.toRadians(getPositionDegrees())
         // TODO: create a double called measurementRadiansPerSecond and initialize to Math.toRadians(getVelocityDegreesPerSecond())
-        // TODO: create a double called setPointRadians and initialize to wrapSetpointAngle(measurementRadians, degrees)
+        // TODO: create a double called setPointRadians and initialize to Math.toRadians(degrees)
+        // TODO: create a double called errorBound and initialize to (pi - -pi) / 2.0
+        // TODO: create a double called difference and intialize MathUtil.inputModulus(setpointRadians - measurementRadians, -errorBound, errorBound)
+        // TODO: reassign setpointRadian to measurementRadians + difference;
         // TODO: create a State called goal and initialize to setpointRadians and 0.0
         // TODO: create a State called current and initialize to measurementRadians and measurementRadiansPerSecond
         // TODO: create a State called achievableSetpoint and initliaze from trapezoidProfile.calculate
@@ -54,15 +57,6 @@ public abstract class Steer {
         // TODO: voltage = MathUtil.clamp(voltage, -12.0, 12.0);
         // TODO: setInputVoltage to voltage
     }
-
-    public double wrapSetpointAngle(double measurementRadians, double degrees) {
-        // TODO: create a Rotation2d called measurement2d and initialize to Rotation2d.fromRadians(measurementRadians)
-        // TODO: create a Rotation2d called setPoint2d and initialize to Rotation2d.fromDegrees(MathUtil.inputModulus(degrees, 0, 360));
-        // TODO: create a Rotation2d called difference2d and initialize to setPoint2d.minus(measurement2d);
-        // TODO: return measurement2d.plus(difference2d).getRadians();
-        return 0.0; //TODO: remove this line when finished
-    }
-
     
     public abstract void setInputVoltage(double voltage);
 
